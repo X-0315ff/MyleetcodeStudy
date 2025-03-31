cbb7cd20ef450fab179356b6f6253e1ff4cf4f87
谎言 <1772943791@qq.com>
2025-03-31 17:20:58 +0800

    gx

--- 437. 路径总和 III.js
+++ 437. 路径总和 III.js
@@ -12,5 +12,20 @@
  * @return {number}
  */
 var pathSum = function(root, targetSum) {
-    
+    if(!root) return 0;
+     // 从当前节点出发的路径数量
+    function countPaths(root, targetSum) {
+      if(!root) return 0
+      let count = 0 
+      if(root.val === targetSum) count = 1
+      // 继续递归左右子树
+      count += countPaths(root.left, targetSum - root.val)
+      count += countPaths(root.right, targetSum - root.val)
+      return count
+    }
+     // 计算当前节点和左右子树的路径数量
+    const res = countPaths(root,targetSum)
+                + pathSum(root.left, targetSum)
+                + pathSum(root.right, targetSum)
+    return res
 };
