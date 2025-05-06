501219710386056165771fab51d007c8686765c1
谎言 <1772943791@qq.com>
2025-05-06 14:43:54 +0800

    nn

---
+++ 334. 递增的三元子序列.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let first = Infinity
+    let second = Infinity
+    for( let i of nums){
+      if(i < first){
+        first = i  // 找最小的
+      }else if(i < second){
+        second = i // 找第二小的
+      }else {
+        return true // 找到了第三个比 first 和 second 都大的数
+      }
+    }
+
+    return false
+};
