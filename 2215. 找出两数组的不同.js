40d48cdccf5c4163ff7e7fe04bb738d681108bfe
谎言 <1772943791@qq.com>
2025-05-25 12:23:39 +0800

    set找不同，filter组数方法

---
+++ 2215. 找出两数组的不同.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    const set1 = new Set(nums1)
+    const set2 = new Set(nums2)
+    const diff1 = [...set1].filter(x => !set2.has(x))
+    const diff2 = [...set2].filter(x => !set1.has(x))
+    return [diff1, diff2]
+    
+};
+nums1 = [1,2,3], nums2 = [2,4,6]
+findDifference(nums1,nums2)
