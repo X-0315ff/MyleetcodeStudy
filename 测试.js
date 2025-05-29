51d91d5a6f9eb4f49b09920548b226ee7267f2ac
谎言 <1772943791@qq.com>
2025-05-29 15:23:17 +0800

    。。

--- 测试.js
+++ 测试.js
@@ -1,4 +1,5 @@
-let a = [1, 2, new Set([3, 4]), [5], [6, [7, 8]]]
-console.log(a.flat(), 111) // 不加参数默认是1
-console.log(a.flat(0), 222) // 相当于浅拷贝
-console.log(a.flat(2), 333)   
+// let a = [1, 2, new Set([3, 4]), [5], [6, [7, 8]]]
+// console.log(a.flat(), 111) // 不加参数默认是1
+// console.log(a.flat(0), 222) // 相当于浅拷贝
+// console.log(a.flat(2), 333)   
+console.log(Object.prototype);
