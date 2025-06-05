a2d385f7d4194ff4fc5cff582653a769af3eb54e
谎言 <1772943791@qq.com>
2025-06-06 01:18:36 +0800

    增加注释

--- 328. 奇偶链表.js
+++ 328. 奇偶链表.js
@@ -9,14 +9,17 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// 奇数偶数拆分法
 var oddEvenList = function(head) {
     if(!head) return null
     // odd 奇数， even偶数
     let oddHead = head, evenHead = head.next
     let odd = oddHead,even = evenHead
     while(even && even.next){
+      // 链接奇数
       odd.next =even.next
       odd = odd.next
+      // 链接偶数
       even.next = odd.next
       even = even.next
     }
